cdfb751375535ff9e58556cbefdf173c619cfb54
Bach <50036032+DarkDrake132@users.noreply.github.com>
2021-10-26 22:09:33 +0700

    add image icon user

--- component/Header/Header.js
+++ component/Header/Header.js
@@ -1,19 +1,18 @@
-import { Container, Navbar, Button } from 'react-bootstrap';
+import { Container, Navbar, Button, Image } from 'react-bootstrap';
 import { useRouter } from 'next/router';
 
 function Header({ createBoard }) {
     const router = useRouter();
 
     return (
-        <Navbar bg="primary" variant="dark">
+        <Navbar bg="primary" variant="dark" style={{ fontWeight: 'bold' }}>
             <Container>
                 <Navbar.Brand style={{ cursor: 'pointer' }} onClick={() => { router.push('/') }}>Chello</Navbar.Brand>
                 <Navbar.Toggle />
                 <Button onClick={createBoard} variant="info">Create Room</Button>
                 <Navbar.Collapse className="justify-content-end">
-                <Navbar.Text>
-                    Signed in as: Bách
-                </Navbar.Text>
+                <Image src={`https://png.pngtree.com/png-vector/20190225/ourmid/pngtree-vector-female-avatar-icon-png-image_702460.jpg`} alt="Bach" width="30rem" height="30rem" roundedCircle />
+                <Navbar.Text style={{ marginLeft: '1rem', color: 'white'}}>Bach Donny</Navbar.Text>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
