1d2ad588dee84038e352f0e5ebccf2b250b54a94
Bach <50036032+DarkDrake132@users.noreply.github.com>
2021-10-27 09:23:46 +0700

    change click to focus on user

--- component/Header/Header.js
+++ component/Header/Header.js
@@ -6,7 +6,7 @@ function Header({ createBoard }) {
 
     const popover = (
         <Popover id="popover-basic">
-          <Popover.Header as="h3">Popover right</Popover.Header>
+          <Popover.Header as="h3">Account actions</Popover.Header>
           <Popover.Body>
             <Button variant="danger">Log out</Button>
           </Popover.Body>
@@ -20,7 +20,7 @@ function Header({ createBoard }) {
                 <Navbar.Toggle />
                 <Button onClick={createBoard} variant="info">Create Room</Button>
                 <Navbar.Collapse className="justify-content-end">
-                <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
+                <OverlayTrigger trigger="focus" placement="bottom" overlay={popover}>
                     <Button>
                         <Image src={`https://png.pngtree.com/png-vector/20190225/ourmid/pngtree-vector-female-avatar-icon-png-image_702460.jpg`} alt="Bach" width="30rem" height="30rem" roundedCircle />
                         <Navbar.Text style={{ marginLeft: '1rem', color: 'white'}}>Bach Donny</Navbar.Text>
