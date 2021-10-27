0f13e3b385ee055f635b1be0e35f7c504f7f6f08
Bach <50036032+DarkDrake132@users.noreply.github.com>
2021-10-27 09:17:17 +0700

    add Log out drop down

--- component/Header/Header.js
+++ component/Header/Header.js
@@ -1,8 +1,17 @@
-import { Container, Navbar, Button, Image } from 'react-bootstrap';
+import { Container, Navbar, Button, Image, Popover, OverlayTrigger } from 'react-bootstrap';
 import { useRouter } from 'next/router';
 
 function Header({ createBoard }) {
     const router = useRouter();
+
+    const popover = (
+        <Popover id="popover-basic">
+          <Popover.Header as="h3">Popover right</Popover.Header>
+          <Popover.Body>
+            <Button variant="danger">Log out</Button>
+          </Popover.Body>
+        </Popover>
+    );
 
     return (
         <Navbar bg="primary" variant="dark" style={{ fontWeight: 'bold' }}>
@@ -11,8 +20,12 @@ function Header({ createBoard }) {
                 <Navbar.Toggle />
                 <Button onClick={createBoard} variant="info">Create Room</Button>
                 <Navbar.Collapse className="justify-content-end">
-                <Image src={`https://png.pngtree.com/png-vector/20190225/ourmid/pngtree-vector-female-avatar-icon-png-image_702460.jpg`} alt="Bach" width="30rem" height="30rem" roundedCircle />
-                <Navbar.Text style={{ marginLeft: '1rem', color: 'white'}}>Bach Donny</Navbar.Text>
+                <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
+                    <Button>
+                        <Image src={`https://png.pngtree.com/png-vector/20190225/ourmid/pngtree-vector-female-avatar-icon-png-image_702460.jpg`} alt="Bach" width="30rem" height="30rem" roundedCircle />
+                        <Navbar.Text style={{ marginLeft: '1rem', color: 'white'}}>Bach Donny</Navbar.Text>
+                    </Button>
+                </OverlayTrigger>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
