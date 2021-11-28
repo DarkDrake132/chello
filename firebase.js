b9853036faa773e3f5035ef5b26dd5c73b3351d0
Bach <50036032+DarkDrake132@users.noreply.github.com>
2021-11-28 21:18:48 +0700

    add firebase and firebase-cli

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBGVnL4s696zBN0Ze-bS4KHml_VUJqV5JI",
+    authDomain: "chello-dea9c.firebaseapp.com",
+    projectId: "chello-dea9c",
+    storageBucket: "chello-dea9c.appspot.com",
+    messagingSenderId: "47794434128",
+    appId: "1:47794434128:web:90abdb539fc7bb5c33a155",
+    measurementId: "G-D6WCC6XWV8"
+  };
