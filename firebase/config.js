a27d0042e29d670da323d71b5a84e4804aea3f2b
Bach <50036032+DarkDrake132@users.noreply.github.com>
2021-12-19 22:51:47 +0700

    create google login function (click on google button)

---
+++ firebase/config.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBGVnL4s696zBN0Ze-bS4KHml_VUJqV5JI",
+  authDomain: "chello-dea9c.firebaseapp.com",
+  projectId: "chello-dea9c",
+  storageBucket: "chello-dea9c.appspot.com",
+  messagingSenderId: "47794434128",
+  appId: "1:47794434128:web:90abdb539fc7bb5c33a155",
+  measurementId: "G-D6WCC6XWV8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const auth = getAuth();
+const db = getFirestore();
+
+export { auth, db }
--- pages/login.js
+++ pages/login.js
@@ -2,7 +2,37 @@ import { Fragment } from "react";
 import Image from "next/image";
 import Head from "next/head";
 
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+import { auth } from "../firebase/config";
+
+const googleProvider = new GoogleAuthProvider();
+
 export default function Login() {
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        console.log(token, user)
+        auth.signOut();
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+
   return (
     <Fragment>
       <Head>
@@ -138,7 +168,7 @@ export default function Login() {
                     className="login100-social-item bg3"
                     style={{ textDecoration: "none" }}
                   >
-                    <i className="fa fa-google"></i>
+                    <i className="fa fa-google" onClick={handleGoogleLogin}></i>
                   </a>
                 </div>
 
